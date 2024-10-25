292ee83027fe93b04002e56cd84103787b60249c
milocarter12 <37109701-milocarter12@users.noreply.replit.com>
2024-10-25 21:24:38 +0000

    Agent query: Is the Streamlit app accessible and running correctly?

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ if __name__ == "__main__":
     os.environ['STREAMLIT_SERVER_PORT'] = '5000'
     os.environ['STREAMLIT_SERVER_ADDRESS'] = '0.0.0.0'
     os.environ['STREAMLIT_SERVER_HEADLESS'] = 'true'
-    os.environ['STREAMLIT_SERVER_ENABLE_XSRF_PROTECTION'] = 'false'  # Disable XSRF to allow CORS
-    os.environ['STREAMLIT_SERVER_ENABLE_CORS'] = 'true'
+    os.environ['STREAMLIT_SERVER_ENABLE_XSRF_PROTECTION'] = 'true'  # Enable XSRF protection
+    os.environ['STREAMLIT_SERVER_ENABLE_CORS'] = 'true'  # Enable CORS
     os.environ['STREAMLIT_BROWSER_GATHER_USAGE_STATS'] = 'false'
     
     try:
@@ -19,8 +19,8 @@ if __name__ == "__main__":
                 'server.port': 5000,
                 'server.address': '0.0.0.0',
                 'server.headless': True,
-                'server.enableXsrfProtection': False,  # Disable XSRF to allow CORS
-                'server.enableCORS': True,
+                'server.enableXsrfProtection': True,  # Enable XSRF protection
+                'server.enableCORS': True,  # Enable CORS
                 'browser.gatherUsageStats': False
             }
         )
