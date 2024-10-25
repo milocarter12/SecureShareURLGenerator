bdd977690bd537eba1ef5cd5e2e0dafa44d58bc1
milocarter12 <37109701-milocarter12@users.noreply.replit.com>
2024-10-25 18:39:28 +0000

    Restored to 'ecaf3c167919fec62639e8b3d202d4b1a49e32e1'

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, session, jsonify
+from flask import Flask, render_template, request, redirect, url_for, session
 from flask_socketio import SocketIO, emit
 from functools import wraps
 from datetime import datetime
